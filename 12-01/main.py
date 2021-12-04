c51965751f7a168cd5519e74aab1a60d7964475b
Vivian Pan <vivian.pan@unity3d.com>
2021-12-03 23:47:08 -0800

    part2 of solution

--- 12-01/main.py
+++ 12-01/main.py
@@ -1,17 +1,27 @@
 from argparse import ArgumentParser
 import os.path
 
-def sonar_sweep(input_file):
+def store_input(input_file):
     with open(input_file) as f:
         measurements = f.readlines()
 
     # convert string to int
     measurements = [int(i) for i in measurements]
+    return measurements
 
+def sonar_sweep(measurements):
     count = 0
     # loop through current measurement and next with zip as pairs
     for c, n in zip(measurements, measurements[1:]):
         if c < n:
+            count += 1
+
+    print("Sonar sweep found %d measurements that are larger than the previous measurement" % count)
+
+def sonar_sweep_window(measurements, window_size):
+    count = 0
+    for i in range(window_size, len(measurements)):
+        if measurements[i-window_size] < measurements[i]:
             count += 1
 
     print("Sonar sweep found %d measurements that are larger than the previous measurement" % count)
@@ -26,6 +36,10 @@ if __name__ == '__main__':
     parser.add_argument("-i", "--input",
         dest="filename", required=True, type=extant_file,
         help="input file with sonar sweep measurements", metavar="FILE")
+    parser.add_argument("-w", "--window-size",
+        dest="WS", required=True, help="window size for sonar sweep")
 
     args = parser.parse_args()
-    sonar_sweep(args.filename)
+    input = store_input(args.filename)
+    sonar_sweep(input)
+    sonar_sweep_window(input, int(args.WS))
